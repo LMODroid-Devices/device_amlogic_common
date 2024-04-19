22355ad1fb0d18e65fce6d774c05cd6956f08ae9
LMO GerritBot <gerrit@libremobileos.com>
2024-04-19 19:32:11 +0000

    Adapt to LMODroid by device-add script

--- amlogic.mk
+++ amlogic.mk
@@ -136,7 +136,7 @@ PRODUCT_COPY_FILES +=  \
 ## Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 ifneq ($(PRODUCT_IS_ATV),true)
 DEVICE_PACKAGE_OVERLAYS += \
